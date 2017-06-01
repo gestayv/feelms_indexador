1e193d6eeb8d5212301b26abab8d607195967618
Arturo Salinas <arturo.salinas.dt@gmail.com>
2017-06-01 15:13:19 +0300

    Respaldando avance

--- src/main/java/indexer/SentimentAnalyzer.java
+++ src/main/java/indexer/SentimentAnalyzer.java
@@ -1,5 +1,12 @@
 package indexer;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -7,14 +14,35 @@ import java.util.Map;
  */
 public class SentimentAnalyzer {
 
-    private Map<String, Double> dictionary;
+    private Map<String, Double> dictionary; //Para palabras enteras
+    private Map<String, Double> suffixDictionary; //Aquellas que estan guardadas solo como sufijo
 
     private SentimentAnalyzer() {
 
     }
 
-    public SentimentAnalyzer(String filePath) {
+    public SentimentAnalyzer(String filePath) throws IOException {
 
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+
+            List<String> lines = new ArrayList<String>();
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                lines.add(line.trim());
+            }
+
+            int size = lines.size();
+            Double cap = (size / 0.75) + 1;
+            dictionary = new HashMap<String, Double>(cap.intValue());
+
+            for (String auxLine: lines) {
+                String[] lineSplit = auxLine.split(",");
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.print(e.getMessage());
+            throw e;
+        }
     }
 
 }
