06ee86bb9ba906b2fd6b6d45105e01c1087b0a23
Arturo Salinas <arturo.salinas.dt@gmail.com>
2017-06-04 21:01:57 +0300

    Modificado para que agregue sentiments a tweet_counts. Limpiar

--- src/main/java/db/TweetCount.java
+++ src/main/java/db/TweetCount.java
@@ -13,25 +13,58 @@ public class TweetCount {
     private int film_id;
     private LocalDate date;
     private int count;
+    private int pos;
+    private int neg;
 
     private TweetCount() {
+
     }
 
-    public TweetCount(int film_id, LocalDate date, int count) {
+    public TweetCount(int film_id, LocalDate date, int count, int pos, int neg) {
         this.film_id = film_id;
         this.date = date;
         this.count = count;
+        this.pos = pos;
+        this.neg = neg;
     }
 
     public int getFilm_id() {
         return film_id;
     }
 
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
+
     public LocalDate getDate() {
         return date;
     }
 
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
     public int getCount() {
         return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public int getNeg() {
+        return neg;
+    }
+
+    public void setNeg(int neg) {
+        this.neg = neg;
     }
 }
